c426b4af715feb5479cb071ed3e7a943cccf4a3c
Green Sky <green@g-s.xyz>
2022-07-02 16:36:55 +0200

    start with meta setup, seperate tox main

--- src/components/path_progress.hpp
+++ src/components/path_progress.hpp
@@ -2,11 +2,15 @@
 
 #include <cstdint>
 
+#include <nlohmann/json.hpp>
+
 namespace mini_td::Components {
 
 struct PathProgress {
 	float progress {0.f}; // [0,1]
 };
+
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(PathProgress, progress)
 
 } // mini_td::Components
 
